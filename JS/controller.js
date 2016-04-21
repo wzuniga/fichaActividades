07679a56793f361bc2464b49d79a7c3cc63d8d4f
alumno <alumno@lab01-02.(none)>
2016-04-21 11:21:19 -0500

    Adding delete button

--- JS/controller.js
+++ JS/controller.js
@@ -3,6 +3,7 @@ app.controller('ctrl', function($scope, $compile) {
     
     $scope.test = "gg";
     $scope.tr = "gg2";
+    $scope.c3 = "";
 
     //var getMeMyValue = $scope[$scope.courses[0].value];
     //var r= $compile("$scope.test")
@@ -10,6 +11,10 @@ app.controller('ctrl', function($scope, $compile) {
     //console.log($scope.courses);
     //console.log(getMeMyValue);
     $scope.countCourses = 0;
+    
+    var myEl = angular.element( document.querySelector( '#v1' ) );
+    myEl.remove();
+
 
     $scope.addCourse = function() {
 
@@ -17,19 +22,22 @@ app.controller('ctrl', function($scope, $compile) {
 
         //window.location.reload();
         //window.alert("hi!");
-        var inp = '<td rowspan="2"><input type="text" class="form-control rectif" ng-model="';
+        var inp = '<td rowspan="2"><input type="text" class="form-control rectif rectif-letter" ng-model="';
         var skt = '<td><input type="text" class="form-control rectif" ng-model="';
         var models = ['course', 'sem', 'school', 't', 'p', 'l', 'hshare', 'hweek', 'htotal', 'nroom', 'group', 'nstudent', 'ld', 'md', 'mid', 'jd', 'vd', 'lh', 'mh', 'mih', 'jh', 'vh'];
         var placeh = ['Curso','','','','','','','','','','','','','','','','','','','','','']
-        var str = '<tr>';
-      
-        for (var i = 0; i < 12; i++)
+        var str = '<tr id="courseLine'+$scope.countCourses+'">';
+      	
+	var temp = '<td rowspan="2"><textarea class="form-control rectif" ng-model="'; 
+	str += temp+models[0]+$scope.countCourses+'" placeholder="'+placeh[0]+'"></textarea></td>';
+
+        for (var i = 1; i < 12; i++)
             str += inp+models[i]+$scope.countCourses+'" placeholder="'+placeh[i]+'"></td>';
         str+='<td>De: </td>';
 
         for (var i = 12; i < 17; i++)
             str += skt+models[i]+$scope.countCourses+'" placeholder="'+placeh[i]+'"></td>';
-        str+='</tr><tr><td>Hasta: </td>';
+        str+='</tr><tr id="courseLine'+$scope.countCourses+'"><td>Hasta: </td>';
       
         for (var i = 17; i < 22; i++)
             str += skt+models[i]+$scope.countCourses+'" placeholder="'+placeh[i]+'"></td>';
@@ -41,10 +49,13 @@ app.controller('ctrl', function($scope, $compile) {
         var getMeMyValue = $scope["course"+($scope.countCourses-1)];
         //var getMeMyValue = $scope[$scope.courses[$scope.countCourses-1].value];
         console.log(getMeMyValue);
+
     };
 
-    $scope.ttt = function() {
-        console.log("botoncito");
+    $scope.removeCourse = function() {
+        angular.element( document.querySelector( "#courseLine"+$scope.countCourses ) ).remove();
+        angular.element( document.querySelector( "#courseLine"+$scope.countCourses ) ).remove();
+        $scope.countCourses--;
     }
 
 });
